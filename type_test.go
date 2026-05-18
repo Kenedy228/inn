1d9269b5576c6e877e80b768d6abd22664d2ed55
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-18 14:40:27 +1000

    добавил тесты на валидацию типа ИНН

---
+++ type_test.go
@@ -0,0 +1,39 @@
+package inn
+
+import "testing"
+
+func TestType_isValid(t *testing.T) {
+	tests := []struct {
+		name string
+		t    Type
+		want bool
+	}{
+		{
+			name: "невалидный тип",
+			t:    Type("invalid"),
+			want: false,
+		},
+		{
+			name: "пустой тип",
+			t:    Type(""),
+			want: false,
+		},
+		{
+			name: "пустая строка",
+			t:    "",
+			want: false,
+		},
+		{
+			name: "валидный тип",
+			t:    TypeIP,
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.t.isValid(); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
