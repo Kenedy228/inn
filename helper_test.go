afb700b8daedba75cb48b4952d7232e11cea7503
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-18 18:55:13 +1000

    добавил тесты на функцию parseDigits

--- helper_test.go
+++ helper_test.go
@@ -1,6 +1,10 @@
 package inn
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func Test_isDigit(t *testing.T) {
 	type args struct {
@@ -44,6 +48,66 @@ func Test_isDigit(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isDigit(tt.args.r); got != tt.want {
 				t.Errorf("isDigit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseDigits(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		want      []int
+		wantPanic bool
+	}{
+		{
+			name: "парсинг строки из всех нулей",
+			args: args{
+				s: strings.Repeat("0", 20),
+			},
+			want:      make([]int, 20),
+			wantPanic: false,
+		},
+		{
+			name: "парсинг строки с разными цифрами",
+			args: args{
+				s: "94321",
+			},
+			want:      []int{9, 4, 3, 2, 1},
+			wantPanic: false,
+		},
+		{
+			name: "парсинг пустой строки",
+			args: args{
+				s: "",
+			},
+			want:      []int{},
+			wantPanic: false,
+		},
+		{
+			name: "парсинг строки с нечисловым значением",
+			args: args{
+				s: "abcd123",
+			},
+			want:      []int{},
+			wantPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantPanic {
+				defer func() {
+					if r := recover(); r == nil {
+						t.Errorf("expected panic but did not panic")
+					}
+				}()
+			}
+
+			if got := parseDigits(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseDigits() = %v, want %v", got, tt.want)
 			}
 		})
 	}
