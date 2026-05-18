50532301108223be261823e195c020765f18b896
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-18 18:55:08 +1000

    добавил функцию parseDigits

--- helper.go
+++ helper.go
@@ -3,3 +3,17 @@ package inn
 func isDigit(r rune) bool {
 	return '0' <= r && r <= '9'
 }
+
+func parseDigits(s string) []int {
+	digits := make([]int, 0, len(s))
+
+	for _, r := range s {
+		if !isDigit(r) {
+			panic("")
+		}
+
+		digits = append(digits, int(r-'0'))
+	}
+
+	return digits
+}
