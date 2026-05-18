480c1a47015f8dbb0feb007f18c292d50efa5dfa
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-19 00:31:06 +1000

    добавил функцию validatePersonINN

--- validator.go
+++ validator.go
@@ -1,1 +1,33 @@
 package inn
+
+import (
+	"fmt"
+)
+
+func validatePersonINN(inn string) error {
+	digits := parseDigits(inn)
+
+	firstPartDigits := digits[:10]
+	expectedFirstPartDigit := digits[10]
+
+	if err := validateChecksumDigit(
+		firstPartDigits,
+		personFirstChecksumWeights,
+		expectedFirstPartDigit,
+	); err != nil {
+		return fmt.Errorf("")
+	}
+
+	secondPartDigits := digits[:11]
+	expectedSecondPartDigit := digits[11]
+
+	if err := validateChecksumDigit(
+		secondPartDigits,
+		personSecondChecksumWeights,
+		expectedSecondPartDigit,
+	); err != nil {
+		return fmt.Errorf("")
+	}
+
+	return nil
+}
