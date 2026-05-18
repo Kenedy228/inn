ba6b89ce90f7ba7390901bdd642508c95cfd8435
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-18 14:55:17 +1000

    добавил метод checksumCoefficients для получения коэффициентов чек суммы

--- type.go
+++ type.go
@@ -27,3 +27,19 @@ func (t Type) codeLength() int {
 		return 0
 	}
 }
+
+func (t Type) checksumCoefficients() [][]int {
+	switch t {
+	case TypeIP, TypePhysical:
+		return [][]int{
+			{7, 2, 4, 10, 3, 5, 9, 4, 6, 8},
+			{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8},
+		}
+	case TypeOrganization:
+		return [][]int{
+			{2, 4, 10, 3, 5, 9, 4, 6, 8},
+		}
+	default:
+		return [][]int{}
+	}
+}
