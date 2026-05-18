a5ae7bfdcbbb1a081610fb43aa14dfe7747e93cf
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-18 17:08:23 +1000

    добавил инвариант в функцию validateDigitsOnly что строка не может быть пустой

--- validator_helper.go
+++ validator_helper.go
@@ -5,6 +5,10 @@ import "fmt"
 func validateDigitsOnly(code string) error {
 	runes := []rune(code)
 
+	if len(runes) == 0 {
+		return fmt.Errorf("")
+	}
+
 	for _, r := range runes {
 		if isDigit(r) {
 			continue
