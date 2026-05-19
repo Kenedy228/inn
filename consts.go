4a8a61efbc0efe1fa4bc9ec778d5c8f0d06e7080
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-19 10:28:25 +1000

    сделал ренейминг констант

--- consts.go
+++ consts.go
@@ -1,23 +1,23 @@
 package inn
 
 const (
-	legalEntityINNDigitsCount            = 10
-	naturalPersonINNDigitsCount          = 12
-	individualEntrepreneurINNDigitsCount = 12
+	legalEntityDigitsCount            = 10
+	naturalPersonDigitsCount          = 12
+	individualEntrepreneurDigitsCount = 12
 )
 
 var (
-	naturalPersonFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
-	naturalPersonSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	naturalPersonFirstWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	naturalPersonSecondWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 
-	individualEntrepreneurFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
-	individualEntrepreneurSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	individualEntrepreneurFirstWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	individualEntrepreneurSecondWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 
-	legalEntityChecksumWeights = []int{2, 4, 10, 3, 5, 9, 4, 6, 8}
+	legalEntityWeights = []int{2, 4, 10, 3, 5, 9, 4, 6, 8}
 )
 
 var (
-	naturalPersonForbiddenINN          = "000000000000"
-	individualEntrepreneurForbiddenINN = "000000000000"
-	legalEntityForbiddenINN            = "0000000000"
+	naturalPersonForbiddenValue          = "000000000000"
+	individualEntrepreneurForbiddenValue = "000000000000"
+	legalEntityForbiddenValue            = "0000000000"
 )
