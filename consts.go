5e14ae303f32942a8eb7f615621610228574e763
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-19 10:05:50 +1000

    добавил константы для ИП

--- consts.go
+++ consts.go
@@ -1,12 +1,17 @@
 package inn
 
 const (
-	legalEntityINNDigitsCount = 10
-	personINNDigitsCount      = 12
+	legalEntityINNDigitsCount            = 10
+	naturalPersonINNDigitsCount          = 12
+	individualEntrepreneurINNDigitsCount = 12
 )
 
 var (
 	personFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 	personSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
-	legalEntityChecksumWeights  = []int{2, 4, 10, 3, 5, 9, 4, 6, 8}
+
+	individualEntrepreneurFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	individualEntrepreneurSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+
+	legalEntityChecksumWeights = []int{2, 4, 10, 3, 5, 9, 4, 6, 8}
 )
