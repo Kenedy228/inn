964f785cd8a3e0b8ecd6bb4bd4143c6905366813
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-19 10:13:25 +1000

    добавил запрещенные значения и разделил ИП от ФЛ

--- consts.go
+++ consts.go
@@ -7,11 +7,17 @@ const (
 )
 
 var (
-	personFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
-	personSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	naturalPersonFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
+	naturalPersonSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 
 	individualEntrepreneurFirstChecksumWeights  = []int{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 	individualEntrepreneurSecondChecksumWeights = []int{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}
 
 	legalEntityChecksumWeights = []int{2, 4, 10, 3, 5, 9, 4, 6, 8}
+)
+
+var (
+	naturalPersonForbiddenINN          = "000000000000"
+	individualEntrepreneurForbiddenINN = "000000000000"
+	legalEntityForbiddenINN            = "0000000000"
 )
